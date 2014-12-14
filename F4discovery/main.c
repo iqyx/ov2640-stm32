5573a19397bed9ca7472857310ca6ebfec4b6545
Marek Koza <qyx@krtko.org>
2014-12-14 03:57:07 +0100

    Add i2c support and OV2640 register reading.

--- F4discovery/main.c
+++ F4discovery/main.c
@@ -4,12 +4,53 @@
 MemoryHeap ccm_heap;
 
 
+static const I2CConfig i2cfg1 = {
+	OPMODE_I2C,
+	100000,
+	FAST_DUTY_CYCLE_2,
+};
+
+
+
+uint32_t sccb_read_reg(uint8_t addr, uint8_t reg, uint8_t *value) {
+
+	systime_t timeout = MS2ST(4);
+	uint8_t txbuf[2] = {reg, 0x00};
+	uint8_t rxbuf[2];
+
+	i2cAcquireBus(&I2CD1);
+	msg_t status = i2cMasterTransmitTimeout(&I2CD1, addr, txbuf, 1, rxbuf, 1, timeout);
+	i2cReleaseBus(&I2CD1);
+
+	if (status != RDY_OK){
+		return -1;
+	}
+
+	*value = rxbuf[0];
+
+	return 0;
+}
+
+
+
 static msg_t Thread1(void *arg) {
 	(void)arg;
 
 	chRegSetThreadName("test");
 
+	/* I2C1_SDA */
+	palSetPadMode(GPIOB, 7, PAL_MODE_ALTERNATE(4));
+	/* I2C1_SCL */
+	palSetPadMode(GPIOB, 8, PAL_MODE_ALTERNATE(4));
+
+	i2cStart(&I2CD1, &i2cfg1);
+
 	while (1) {
+		uint8_t vid;
+		if (sccb_read_reg(0x60, 0x0a, &vid) == 0) {
+			palTogglePad(GPIOD, 14);
+		}
+
 		palTogglePad(GPIOD, 15);
 		chThdSleepMilliseconds(50);
 	}
@@ -35,7 +76,9 @@ int main(void) {
 	palClearPad(GPIOD, 14);
 	palClearPad(GPIOD, 15);
 
-	chThdCreateFromHeap(&ccm_heap, 37000, NORMALPRIO, Thread1, NULL);
+
+
+	chThdCreateFromHeap(&ccm_heap, 4000, NORMALPRIO, Thread1, NULL);
 
 	while (1) {
 		chThdSleepMilliseconds(500);
